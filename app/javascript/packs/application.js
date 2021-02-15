89da016dfbaa426bf51eeb15359b5bc14e704319
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2021-02-15 16:25:15 +0100

    setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,13 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// External imports
+import "bootstrap";
+
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+});
