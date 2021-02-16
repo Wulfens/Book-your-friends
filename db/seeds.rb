c596a44607c88a888c3b891562e4de536d5dbda1
Thomas Marignan <marignanthomas10@gmail.com>
2021-02-16 16:41:12 +0100

    front dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 require 'faker'
+
+User.destroy_all
+Animal.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,12 +10,14 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: "test@hotmal.fr", password: "cat123", first_name: "Jean" ,last_name: "Dog")
+User.create(email: "test@hotmail.fr", password: "cat123", first_name: "Jean" ,last_name: "Dog")
+
 20.times do 
   Animal.create([{
     user: User.first,
     name: Faker::Creature::Cat.name,
     species: Faker::Creature::Animal.name,
     price_per_hour: Faker::Number.within(range: 1..15)
+    
   }])
 end
