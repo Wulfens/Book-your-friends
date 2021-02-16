80964a52cbcd07e2be85e61fda5a210964ef7106
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2021-02-16 11:02:34 +0100

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :animals, only: [ :index, :new, :create, :destroy] do
+  resources :animals, only: [ :index, :show, :new, :create, :destroy ] do
     resources :locations, only: [ :new, :create ] do
       resources :reviews, only: [ :new, :create ]
     end
