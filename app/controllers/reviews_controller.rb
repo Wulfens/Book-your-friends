eae03789c86c3e9eb08853e693d54482f3a7dff8
Estelle Bousquet <estellebousquet@gmail.com>
2021-02-16 15:30:12 +0100

    destoy show locations routes changed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @animal = Animal.find(params[:animal_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @animal = AnimaL.find(params[:animal_id])
+    @review.animal = @animal
+    if @review.save
+      redirect_to animal_path(@animal)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :animals, only: [ :index, :show, :new, :create, :destroy ] do
-    resources :locations, only: [ :new, :create ] do
-      resources :reviews, only: [ :new, :create ]
-    end
+    resources :locations, only: [ :new, :create ]
   end
 
-  resources :locations, only: [ :show ]
-
+  resources :locations, only: [ :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
