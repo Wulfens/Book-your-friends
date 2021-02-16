c9740b020f0cb90e4a2aeb71a2df07f4809703a0
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-02-16 12:43:01 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "test@hotmal.fr", password: "cat123", first_name: "Jean" ,last_name: "Dog")
+20.times do 
+  Animal.create([{
+    user: User.first,
+    name: Faker::Creature::Cat.name,
+    species: Faker::Creature::Animal.name,
+    price_per_hour: Faker::Number.within(range: 1..15)
+  }])
+end
