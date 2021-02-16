5df6663ab33b751c4ecd4ddd5178c5abd9560828
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-02-16 15:59:05 +0100

    Dashboard controller + show crées

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ require 'faker'
 
 
 User.create(email: "test@hotmal.fr", password: "cat123", first_name: "Jean" ,last_name: "Dog")
+User.create(email: "user2@hotmail.fr", password: "cat123", first_name: "Hot" ,last_name: "Dog")
 20.times do 
   Animal.create([{
     user: User.first,
