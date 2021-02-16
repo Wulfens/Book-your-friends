65e551c5ba81ceb869b5c12650a8e27e0c0b2717
Thomas Marignan <marignanthomas10@gmail.com>
2021-02-16 17:46:11 +0100

    front !

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 require 'faker'
+require "open-uri"
+
+file = URI.open('https://images.unsplash.com/photo-1574158622682-e40e69881006?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1700&q=80')
 
 User.destroy_all
 Animal.destroy_all
@@ -12,13 +15,14 @@ Animal.destroy_all
 
 
 User.create(email: "test@hotmal.fr", password: "cat123", first_name: "Jean" ,last_name: "Dog")
-User.create(email: "user2@hotmail.fr", password: "cat123", first_name: "Hot" ,last_name: "Dog")
-20.times do 
-  Animal.create([{
-    user: User.first,
-    name: Faker::Creature::Cat.name,
-    species: Faker::Creature::Animal.name,
-    price_per_hour: Faker::Number.within(range: 1..15)
-    
-  }])
-end
+User.create(email: "user2@hotmail.fr", password: "cat123", first_name: "Hot" ,last_name: "Dog") 
+
+animal = Animal.new({
+  user: User.first,
+  name: Faker::Creature::Cat.name.upcase,
+  species: Faker::Creature::Animal.name,
+  price_per_hour: Faker::Number.within(range: 1..15)
+})
+
+animal.photo.attach(io: file, filename: 'cat.webp', content_type: 'image/webp')
+animal.save!
