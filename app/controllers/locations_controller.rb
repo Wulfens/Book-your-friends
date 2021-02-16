f685feba41fd35c05beed7360d8fad846d306e9e
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2021-02-16 13:04:26 +0100

    location-new

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,22 @@
 class LocationsController < ApplicationController
+
+    def new
+        @animal = Animal.find(params[:animal_id])
+        @location = Location.new
+    end
+
+    def create
+        @location = location.new(location_params)
+        @animal = Animal.find(params[:animal_id])
+        @location.animal = @animal
+        @location.save
+        redirect_to animal_path(@animal)
+      end
+    
+      private
+    
+      def location_params
+        params.require(:location).permit(:start_date, :end_date)
+      end
+   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mathieu = User.create!(email: "mail@mail.mail", password: "123456")
+Animal.create!(name: "Pepito", species: "Chat", price_per_hour: 15, user: mathieu)
