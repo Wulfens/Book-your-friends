8c98602548ee1a91b0cc0750cd160b565ac64e43
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2021-02-16 10:59:33 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :animals, only: [ :index, :new, :create, :destroy] do
+    resources :locations, only: [ :new, :create ] do
+      resources :reviews, only: [ :new, :create ]
+    end
+  end
+
+  resources :locations, only: [ :show ]
+
 end
