2f0af0865cad898d6bc385c555e8c456659244f1
Estelle Bousquet <estellebousquet@gmail.com>
2021-02-16 12:10:58 +0100

    add index show

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,19 @@
 class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.new
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :price_per_hour)
+  end
 end
