b1c0d2aa819dbf5c2c692eed7b899a4afd37e1be
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2021-02-17 16:14:27 +0100

    c'est la merde

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import "../plugins/flatpickr"
+import {flatPickrInit} from "../plugins/flatpickr"
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -19,4 +20,5 @@ import "../plugins/flatpickr"
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  flatPickrInit();
 });
