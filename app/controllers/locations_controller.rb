b900ca01bb509beb0b8ab5bc1bdebe00520be010
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2021-02-18 22:23:20 +0100

    fix v52

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,8 +11,7 @@ class LocationsController < ApplicationController
         @animal = Animal.find(params[:animal_id])
         @location.animal = @animal
         @location.user = @user
-        @location.save
-        redirect_to animal_path(@animal)
+        redirect_to dashboard_path if @location.save
       end
 
       def show
@@ -21,20 +20,22 @@ class LocationsController < ApplicationController
 
       def accept
         @location = Location.find(params[:id])
-        @location.update(status: "Accepted")
-        redirect_to dashboard_path
+        # @location.update(status: "Accepted")
+        @location.status = "Accepted"
+        redirect_to dashboard_path if @location.save
       end
 
       def decline
         @location = Location.find(params[:id])
-        @location.update(status: "Declined")
-        redirect_to dashboard_path
+        # @location.update(status: "Declined")
+        @location.status = "Declined"
+        redirect_to dashboard_path if @location.save
       end
 
       private
     
       def location_params
-        params.require(:location).permit(:start_date, :end_date, :status)
+        params.require(:location).permit(:start_date, :end_date)
       end
    
 end
