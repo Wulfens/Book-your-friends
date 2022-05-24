c769fc36c1706a17713c8208db0000f231543c06
Mathieu Trancoso <Trancoso.mathieu@gmail.com>
2022-05-24 23:44:55 +0200

    added some shortcuts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # has_many :animals, through: :locations, as: :rented_animals
   has_many :animals, dependent: :destroy
   has_many :locations
+  has_many :rented_animals, through: :locations, source: :animal
+  has_many :animals_required, through: :animals, source: :locations
+  
+  scope :pending, -> { where( status: "pending")}
+
   has_one_attached :photo
   has_one_attached :banner_photo
+
+
+
 end
